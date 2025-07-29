ab1ed3e4b022668476d1f7b416199324ee30a653
navaneethhhk <hknavaneeth@gmail.com>
2025-07-29 21:00:24 +0530

    2907

--- src/test/java/org/example/base/CommonToAllTests.java
+++ src/test/java/org/example/base/CommonToAllTests.java
@@ -1,17 +1,7 @@
 package org.example.base;
 
-import org.example.driver.DriverManager;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+
 
 public class CommonToAllTests {
-    @BeforeMethod
-    public void setUp(){
-        DriverManager.init();
-    }
 
-    @AfterMethod
-    public void tearDown(){
-        DriverManager.down();
-    }
 }
--- src/test/java/org/example/tests/VerifyLogin.java
+++ src/test/java/org/example/tests/VerifyLogin.java
@@ -1,22 +1,7 @@
 package org.example.tests;
 
-import org.example.driver.DriverManager;
-import org.example.page_pom.LoginPage;
-import org.example.utils.PropertiesReader;
-import org.openqa.selenium.WebDriver;
-
-import org.openqa.selenium.edge.EdgeDriver;
-
-import org.openqa.selenium.edge.EdgeOptions;
-import org.testng.annotations.Test;
-
 
 
 public class VerifyLogin {
-    @Test
-    public void verifyLogin(){
 
-        LoginPage loginPage = new LoginPage(DriverManager.getDriver());
-        loginPage.verifyLoginPage(PropertiesReader.readKey("username"),PropertiesReader.readKey("passwprd"));
-    }
 }
