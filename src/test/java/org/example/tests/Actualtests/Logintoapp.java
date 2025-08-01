e2310991cf9501382a7d122b77817d0e4172486e
navaneethhhk <hknavaneeth@gmail.com>
2025-08-01 21:34:02 +0530

    3007

--- src/test/java/org/example/tests/Actualtests/Logintoapp.java
+++ src/test/java/org/example/tests/Actualtests/Logintoapp.java
@@ -6,12 +6,22 @@ import org.example.page_pom.LoginPage;
 import org.example.utils.PropertiesReader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Logintoapp extends CommonToAllTests {
+    private static final Logger logger = LogManager.getLogger(Logintoapp.class);
+
     @Test
     public void logintoamazn(){
+        logger.info("Starting Amazon login test");
+        logger.debug("Creating new LoginPage instance");
+
         LoginPage loginPage = new LoginPage(DriverManager.getDriver());
         String userlogged = loginPage.ValidLogin(PropertiesReader.readKey("username"),PropertiesReader.readKey("password"));
         Assert.assertEquals(userlogged,PropertiesReader.readKey("expecteduser"));
+        logger.info("Login test completed successfully");
+
+
     }
 }
